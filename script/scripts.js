2a7c84315b4bcba105e235d8e14c56f28f09a10c
mauricegreenland <maurice.greenland@gmail.com>
2018-06-06 14:38:47 +0100

    updates

---
+++ script/scripts.js
@@ -0,0 +1,5 @@
+var iconChange = function(){
+    document.getElementById("homeIcon").src = "/myicons/2x/about@2x.png";
+}
+
+document.getElementById("homeIcon").addEventListener("mouseover", iconChange());
